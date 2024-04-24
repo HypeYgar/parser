cefbf31321eed9ae35c106752f8ba190f756e52b
HypeYgar <157480950+HypeYgar@users.noreply.github.com>
2024-04-24 11:59:14 +0300

    Update gorodvnytri.py

--- gorodvnytri.py
+++ gorodvnytri.py
@@ -1,21 +1,30 @@
+import asyncio
 import time
+import traceback
 from selenium import webdriver
-from selenium.common import TimeoutException
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
+from telegram import Bot
 
-def get_last_page_number_from_file(filename):
-    """Функция для чтения последнего значения страницы из файла."""
+def send_telegram_message(bot_token, chat_id, message):
+    """Функция для отправки сообщения в Telegram."""
     try:
-        with open(filename, 'r', encoding='utf-8') as file:
-            lines = file.readlines()
-            if lines:
-                last_line = lines[-1].strip()
-                return int(last_line)
-    except FileNotFoundError:
-        return None
+        bot = Bot(token=bot_token)
+        bot.send_message(chat_id=chat_id, text=message)
+    except Exception as e:
+        print(f"Ошибка при отправке сообщения в Telegram: {e}")
+
+def send_telegram_document(bot_token, chat_id, document_path, caption=""):
+    """Функция для отправки документа в Telegram."""
+    try:
+        bot = Bot(token=bot_token)
+        asyncio.run(bot.send_document(chat_id=chat_id, document=open(document_path, 'rb'), caption=caption))
+    except Exception as e:
+        print(f"Ошибка при отправке документа в Telegram: {e}")
+
 class ELibraryScraper:
     def __init__(self):
         self.driver = webdriver.Chrome()
@@ -24,8 +33,6 @@ class ELibraryScraper:
     def open_proxy_site(self):
         """Открыть прокси-сайт и перейти на гугл через croxyproxy."""
         self.driver.get("https://www.croxyproxy.net/_ru/")
-
-            # Ждем появления ссылки "Гугл" на странице прокси с таймаутом 15 секунд
         google_link = self.wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="quickLinks"]/a[2]')))
         google_link.click()
 
@@ -35,7 +42,7 @@ class ELibraryScraper:
             consent_button = self.wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="L2AGLb"]')))
             consent_button.click()
             time.sleep(5)
-        except:
+        except TimeoutException:
             pass
 
     def search_elibrary(self):
@@ -44,9 +51,7 @@ class ELibraryScraper:
         search_box.send_keys("https://elibrary.ru")
         search_box.send_keys(Keys.ENTER)
         time.sleep(5)
-        # Переход на страницу elibrary.ru
-        elibrary_link = self.wait.until(EC.presence_of_element_located(
-            (By.XPATH, '//*[@id="rso"]/div[1]/div/div/div/div/div/div/div/div[1]/div/span/a/h3')))
+        elibrary_link = self.wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="rso"]/div[1]/div/div/div/div/div/div/div/div[1]/div/span/a/h3')))
         elibrary_link.click()
 
     def login_to_elibrary(self, username, password):
@@ -57,20 +62,16 @@ class ELibraryScraper:
         password_field = self.wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="password"]')))
         password_field.click()
         password_field.send_keys(password)
-        login_button = self.wait.until(
-            EC.presence_of_element_located((By.XPATH, '//*[@id="win_login"]/table[1]/tbody/tr[9]/td/div[2]')))
+        login_button = self.wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="win_login"]/table[1]/tbody/tr[9]/td/div[2]')))
         login_button.click()
-        # Нажать на ссылку для поиска по ГОРОДУ
-        search_button = self.wait.until(
-            EC.element_to_be_clickable((By.XPATH, '//*[@id="win_goto"]/table[1]/tbody/tr[6]/td[2]/a')))
+        search_button = self.wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="win_goto"]/table[1]/tbody/tr[6]/td[2]/a')))
         search_button.click()
         time.sleep(3)
 
     def search_by_GOROD(self, city_list_file):
         """Выполнить поиск по названиям городов и сохранить информацию в файл."""
-        last_processed_line_file = 'last_processed_line.txt'  # Файл для сохранения номера последней обработанной строки
+        last_processed_line_file = 'last_processed_line.txt'
 
-        # Загрузить номер последней обработанной строки, если такой файл существует
         last_processed_line = 0
         try:
             with open(last_processed_line_file, 'r') as f:
@@ -81,37 +82,31 @@ class ELibraryScraper:
             print("Ошибка чтения файла last_processed_line.txt. Начинаем с начала списка.")
 
         with open(city_list_file, 'r', encoding='utf-8') as file:
-            lines = file.readlines()  # Читаем строки из файла
+            lines = file.readlines()
             for i in range(last_processed_line, len(lines)):
-                city_name = lines[i].strip()  # Удаляем лишние пробелы и символы новой строки
+                city_name = lines[i].strip()
 
                 try:
-                    # Найти поле ввода и ввести название города
                     input_field = self.wait.until(EC.presence_of_element_located((By.XPATH,
                                                                                   "/html/body/table/tbody/tr/td/table/tbody/tr/td[2]/table/tbody/tr[3]/td/table[1]/tbody/tr/td[2]/input")))
                     input_field.clear()
                     input_field.send_keys(city_name)
 
-                    # Нажать на вторую кнопку поиска
-                    search_button2 = self.wait.until(
-                        EC.element_to_be_clickable((By.XPATH,
-                                                    '/html/body/table/tbody/tr/td/table/tbody/tr/td[2]/table/tbody/tr[3]/td/table[3]/tbody/tr/td[6]/a')))
+                    search_button2 = self.wait.until(EC.element_to_be_clickable((By.XPATH,
+                                                                                    '/html/body/table/tbody/tr/td/table/tbody/tr/td[2]/table/tbody/tr[3]/td/table[3]/tbody/tr/td[6]/a')))
                     search_button2.click()
 
-                    time.sleep(3)  # Небольшая задержка для загрузки страницы после поиска
+                    time.sleep(3)
 
-                    # Нажать на ссылку для перехода на новую страницу
                     link_to_click = self.wait.until(EC.element_to_be_clickable((By.XPATH,
                                                                                 '/html/body/table/tbody/tr/td/table/tbody/tr/td[2]/table/tbody/tr[3]/td/table[4]/tbody/tr[2]/td[2]/font/a')))
                     link_to_click.click()
 
                     time.sleep(3)
-                    # Найти и сохранить нужные элементы в файл
 
                     with open('city_info.txt', 'a', encoding='utf-8') as info_file:
+                        info_file.write(f"------------------------\n")
                         info_file.write(f"Город: {city_name}\n")
-
-                        # Проверяем и сохраняем полное название
                         full_name_element = None
                         try:
                             full_name_element = self.wait.until(EC.presence_of_element_located((By.XPATH,
@@ -123,8 +118,6 @@ class ELibraryScraper:
                             full_name = full_name_element.text.strip()
                             info_file.write(f"Полное название: {full_name}\n")
                             print(full_name)
-
-                        # Проверяем и сохраняем название на английском
                         english_name_element = None
                         try:
                             english_name_element = self.wait.until(EC.presence_of_element_located((By.XPATH,
@@ -137,7 +130,6 @@ class ELibraryScraper:
                             info_file.write(f"Название на английском: {english_name}\n")
                             print(english_name)
 
-                        # Проверяем и сохраняем сокращение
                         abbreviation_element = None
                         try:
                             abbreviation_element = self.wait.until(EC.presence_of_element_located((By.XPATH,
@@ -150,7 +142,6 @@ class ELibraryScraper:
                             info_file.write(f"Сокращение: {abbreviation}\n")
                             print(abbreviation)
 
-                        # Проверяем и сохраняем сокращение на английском
                         english_abbreviation_element = None
                         try:
                             english_abbreviation_element = self.wait.until(EC.presence_of_element_located((By.XPATH,
@@ -163,7 +154,6 @@ class ELibraryScraper:
                             info_file.write(f"Сокращение на английском: {english_abbreviation}\n")
                             print(english_abbreviation)
 
-                        # Проверяем и сохраняем страну
                         country_element = None
                         try:
                             country_element = self.wait.until(EC.presence_of_element_located((By.XPATH,
@@ -175,21 +165,16 @@ class ELibraryScraper:
                             country = country_element.text.strip()
                             info_file.write(f"Страна: {country}\n")
                             print(country)
-
-                        # Проверяем и сохраняем город
                         city_element = None
                         try:
                             city_element = self.wait.until(EC.presence_of_element_located((By.XPATH,
                                                                                            '/html/body/table/tbody/tr/td/table/tbody/tr/td[2]/table/tbody/tr[3]/td/table/tbody/tr[2]/td[2]/font/a')))
                         except:
                             pass
-
                         if city_element:
                             city = city_element.text.strip()
                             info_file.write(f"Город: {city}\n")
                             print(city)
-
-                        # Проверяем и сохраняем почтовый адрес
                         address_element = None
                         try:
                             address_element = self.wait.until(EC.presence_of_element_located((By.XPATH,
@@ -201,26 +186,27 @@ class ELibraryScraper:
                             address = address_element.text.strip()
                             info_file.write(f"Почтовый адрес: {address}\n\n")
                             print(address)
-
+                        info_file.write(f"------------------------\n")
                         # Сохранить номер строки, с которой начнется следующий запуск
                         with open(last_processed_line_file, 'w') as f:
-                            f.write(str(i + 1))  # Записываем номер следующей строки
-
-                        # Нажать на ссылку для перехода на новую страницу
+                            f.write(str(i + 1))
                         link_to_click = self.wait.until(EC.element_to_be_clickable((By.XPATH,
                                                                                     '/html/body/table/tbody/tr/td/table/tbody/tr/td[1]/table/tbody/tr[6]/td/div/table/tbody/tr/td/table/tbody/tr[6]/td[1]/a')))
                         link_to_click.click()
 
                 except Exception as e:
                     print(f"Ошибка при обработке города '{city_name}': {e}")
+                    send_telegram_document("6988073004:AAGgq7YTG5BUF7P1BM_SFDtIRuLPiJc-8ZE", "-4109363457",
+                                           "city_info.txt", f"Произошла ошибка:")
 
     def quit(self):
         """Закрыть браузер после завершения."""
         self.driver.quit()
+
 def main_script():
     while True:
         try:
-            # Ваш основной код парсинга здесь
+            send_telegram_document("6988073004:AAGgq7YTG5BUF7P1BM_SFDtIRuLPiJc-8ZE", "-4109363457", "city_info.txt", f"браузер включился: ")
             scraper = ELibraryScraper()
             scraper.open_proxy_site()
             scraper.handle_consent_dialog()
@@ -228,8 +214,13 @@ def main_script():
             scraper.login_to_elibrary(username="Evanepon", password="LegoEva210877")
             scraper.search_by_GOROD("kaif.txt")
         except Exception as e:
-            error_message = str(e)
+            print(f"Произошла ошибка: {e}")
+            send_telegram_document("6988073004:AAGgq7YTG5BUF7P1BM_SFDtIRuLPiJc-8ZE", "-4109363457", "city_info.txt", f"Произошла ошибка:")
+        finally:
+            scraper.quit()
+            send_telegram_document("6988073004:AAGgq7YTG5BUF7P1BM_SFDtIRuLPiJc-8ZE", "-4109363457", "city_info.txt", f"браузер выключился: ")
 
-# Вызов функции для выполнения основного скрипта
+
 if __name__ == "__main__":
     main_script()
+
